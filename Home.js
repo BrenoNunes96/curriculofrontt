105d4502efb52302b1238a8cd21bb9715e880d0e
Breno <147736256+BrenoNunes96@users.noreply.github.com>
2024-04-22 16:17:53 -0300

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,69 @@
+import styles from './Home.modules.css'
+export default function Home() {
+  return (
+    <div>
+       <div className='container'>
+
+      <div className='fundo'>
+       <div className='bola1'></div>
+       <div className='bola2'></div>
+       <div className='bola3'> </div>
+    <p>Breno Nunes - Desenvolvedor Front end</p>
+    <div className='Fundo2'> <p>Olá meu nome é </p>  
+  <h1 className='Breno'>Breno </h1>
+  <p>Desenvolvedor Front end</p>
+  <p>
+Sou um apaixonado Front-End Developer, criando experiências digitais cativantes com habilidades em HTML, CSS e JavaScript.<br></br>
+Também possuo conhecimento de React e vue.js. Alem de ter feito bootcamp da dio de angular.
+</p>
+    
+    
+    
+    </div>
+
+ <div className='fundo3'><h1 className="habilidades">Habilidades</h1>
+
+ <img className="html" src="https://cdn-icons-png.flaticon.com/512/919/919827.png" alt="Descrição da imagem" />
+ 
+
+ <img className="html" src='https://w7.pngwing.com/pngs/640/199/png-transparent-javascript-logo-html-javascript-logo-angle-text-rectangle-thumbnail.png'/>
+<img className='html' src=' https://1000logos.net/wp-content/uploads/2020/09/CSS-Logo.jpg'/>
+<img className='html' src=' https://miro.medium.com/v2/resize:fit:900/1*0_zl1YRcPy0ymOpgJ0dQKA.jpeg'/>
+<img className='html' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pZEvDqLnO4ChJd1Cot0f9cTXuatNnaTgF2_zMAr1RA&s'/>
+</div>
+ <div className='coluna1'>
+ <p className='design'></p>
+ 
+ <p className='design2'></p>
+ <p className='design3'></p>
+ <p className='design4'></p>
+ <p className='designx'></p>
+
+
+ </div>
+<div className="fundo4">
+  <a href="https://github.com/BrenoNunes96">  <img src='https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png' className="logos"/>
+
+</a>
+
+  <a href="https://drive.google.com/file/d/1HAnzWtjgyLCi7VK4XEG8wcP5iX3yMRxo/view?usp=drive_link">  <img className='logo' src="https://play-lh.googleusercontent.com/BkRfMfIRPR9hUnmIYGDgHHKjow-g18-ouP6B2ko__VnyUHSi1spcc78UtZ4sVUtBH4g" />
+</a>
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi4JTRBezFPoaXVOvf6o1CGK4EKf5-12uQfNbD7SQ_Ig&s" className="logosx"/>
+</div>
+       <div className="barra">
+        <img src="https://t.ctcdn.com.br/S4OgKKXfk-S-9M0tvzGqQkVkdVY=/i489968.jpeg" className="opera"/>
+        <img src="https://campinas.com.br/wp-content/uploads/2023/09/pipoca-no-cinema-com-almofada-no-chao-scaled-e1695841582534.jpg"
+          className="filme"  />
+          <img src="https://i.pinimg.com/originals/2d/61/d1/2d61d160b98c3f83dd14d0ad9a01584f.jpg" className="todo"/>
+
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2U8m_gt2xFsloEPr0wWUOd6ajz_ttemoo9deNK2KAlrbRm48dRDC4PvKspzpVMi06Q9A&usqp=CAU"
+          
+             className="ciclic"/>
+       </div>
+
+      </div>
+    </div>
+    </div>
+  )
+}
